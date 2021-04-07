9cdec84ccbcd5900be81b278fab59a274f3d6b97
Jose F. Reyes <jose.reyes@prswservices.com>
2021-04-07 17:49:18 -0400

    Set equipment switch panel to 100% height + fixed Y axis label on doser chart.

--- front-end/src/doser/chart.jsx
+++ front-end/src/doser/chart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ResponsiveContainer, Tooltip, YAxis, XAxis, BarChart, Bar } from 'recharts'
 import { fetchDoserUsage } from '../redux/actions/doser'
 import { connect } from 'react-redux'
+import i18next from 'i18next'
 
 class chart extends React.Component {
   constructor (props) {
@@ -38,7 +39,7 @@ class chart extends React.Component {
         <ResponsiveContainer height={this.props.height} width='100%'>
           <BarChart data={this.props.usage.historical}>
             <Bar dataKey='pump' fill='#33b5e5' isAnimationActive={false} />
-            <YAxis label='seconds' />
+            <YAxis label={{ value: i18next.t('second_s'), angle: -90, position: 'insideLeft' }} />
             <XAxis dataKey='time' />
             <Tooltip />
           </BarChart>
