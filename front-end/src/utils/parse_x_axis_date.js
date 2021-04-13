7897193fd7eed7c96d1c14125350fa4d8342db17
Jose F. Reyes <jose.reyes@prswservices.com>
2021-04-13 18:17:36 -0400

    Internationalization of ParseXAxisDate.

--- front-end/src/utils/parse_x_axis_date.js
+++ front-end/src/utils/parse_x_axis_date.js
@@ -1,7 +1,10 @@
+import i18next from 'i18next'
+
 export const ParseXAxisDate = (str) => {
   let parsedDate = str
   if (str !== '' && str !== undefined && typeof str === 'string') {
-    const month = str.substr(0, 3)
+    const strMonth = str.substr(0, 1).toLowerCase() + str.substr(1, 2) // for translation
+    const month = i18next.t(strMonth)
     const day = str.substr(4, 2)
     const hour = str.substr(7, 5)
     const year = str.substring(str.length - 2, str.length)
